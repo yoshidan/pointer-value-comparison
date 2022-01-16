6ab5a2011914c7b063b1cb4075368dec9544bcde
yoshidan <naohiro.y@gmail.com>
2022-01-16 18:20:34 +0900

    format

--- model.go
+++ model.go
@@ -3,10 +3,10 @@ package main
 import "time"
 
 type Sample struct {
-	GroupID 	int64
-	SampleID    int64
-	ItemID 		int64
-	Quantity    int64
-	Name        string
-	Time        time.Time
+	GroupID  int64
+	SampleID int64
+	ItemID   int64
+	Quantity int64
+	Name     string
+	Time     time.Time
 }
